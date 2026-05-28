df846af8f9011e0625e35093954231e15a0d50fa
MetoisTaken <metehanbyz@outlook.com>
2026-05-28 16:18:47 +0300

    release: support common SM8850 stable packages

--- scripts/ci/test-metadata-policy.sh
+++ scripts/ci/test-metadata-policy.sh
@@ -12,9 +12,13 @@ expect_line() {
   fi
 }
 
-expect_line "$RELEASE_ROOT/manifests/pandora-os3.0.309.env" 'KERNEL_COMMON_BRANCH="6.12.23-android16-5-lokumkernel-ksun-susfs-rc1"'
-expect_line "$RELEASE_ROOT/manifests/pandora-os3.0.309.env" 'RELEASE_ID="lokumkernel-xiaomi17pro-6.12.23-ksun-susfs-rc1"'
-expect_line "$RELEASE_ROOT/manifests/pandora-os3.0.309.env" 'RELEASE_ZIP_NAME="LokumKernel-Xiaomi17Pro-6.12.23-KSun-SUSFS-rc1.zip"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'KERNEL_COMMON_BRANCH="6.12.23-android16-5-lokumkernel-ksun-susfs-rc1"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'DEVICE_CODENAME="sm8850"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'DEVICE_MARKETING_NAME="Xiaomi 17 Series"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'SUPPORTED_CODENAMES="pudding pandora popsicle nezha"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'RUNTIME_TESTED_CODENAMES="pandora"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'RELEASE_ID="lokumkernel-sm8850-6.12.23-ksun-susfs-rc1"'
+expect_line "$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" 'RELEASE_ZIP_NAME="LokumKernel-SM8850-6.12.23-KSun-SUSFS-rc1.zip"'
 expect_line "$RELEASE_ROOT/manifests/pandora-os3.0.309-android16-6.12.38-exp.env" 'KERNEL_COMMON_BRANCH="6.12.38-android16-5-lokumkernel-ksun-susfs-exp1"'
 expect_line "$RELEASE_ROOT/manifests/pandora-os3.0.309-android16-6.12.38-exp.env" 'KERNEL_COMMON_HEAD="4131670aae805c1f361a1c8bc15dd3e58ad492a7"'
 expect_line "$RELEASE_ROOT/manifests/pandora-os3.0.309-android16-6.12.38-exp.env" 'RELEASE_ID="lokumkernel-xiaomi17pro-6.12.38-ksun-susfs-exp1"'
@@ -37,14 +41,14 @@ expect_line "$RELEASE_ROOT/templates/anykernel/anykernel.sh.in" 'device.name3=po
 expect_line "$RELEASE_ROOT/templates/anykernel/anykernel.sh.in" 'device.name4=nezha'
 expect_line "$RELEASE_ROOT/templates/anykernel/anykernel.sh.in" 'device.name5='
 
-if rg -n 'ksunext|KernelSUNext|pandora-6\.12\.23-ksun|android16-6\.12-2025-09-ksun|pandora-os3\.0\.309-lokumkernel|LokumKernel-pandora|pandora-os3\.0\.309-android16-6\.12\.38-droidspaces|lokumkernel-xiaomi17pro-6\.12\.38-ksun-susfs-droidspaces|LokumKernel-Xiaomi17Pro-6\.12\.38-KSun-SUSFS-Droidspaces' \
+if rg -n 'ksunext|KernelSUNext|pandora-6\.12\.23-ksun|android16-6\.12-2025-09-ksun|pandora-os3\.0\.309-lokumkernel|LokumKernel-pandora|lokumkernel-xiaomi17pro-6\.12\.23-ksun-susfs|LokumKernel-Xiaomi17Pro-6\.12\.23|pandora-os3\.0\.309-android16-6\.12\.38-droidspaces|lokumkernel-xiaomi17pro-6\.12\.38-ksun-susfs-droidspaces|LokumKernel-Xiaomi17Pro-6\.12\.38-KSun-SUSFS-Droidspaces' \
   "$RELEASE_ROOT/manifests" "$RELEASE_ROOT/docs" >/tmp/lokum-old-metadata-names.txt; then
   echo "old naming still present:" >&2
   cat /tmp/lokum-old-metadata-names.txt >&2
   fail=1
 fi
 
-source_date_epoch="$(env -u SOURCE_DATE_EPOCH MANIFEST="$RELEASE_ROOT/manifests/pandora-os3.0.309.env" bash -c 'source "$0/scripts/common.sh"; printf "%s" "$SOURCE_DATE_EPOCH"' "$RELEASE_ROOT")"
+source_date_epoch="$(env -u SOURCE_DATE_EPOCH MANIFEST="$RELEASE_ROOT/manifests/sm8850-android16-6.12.23-ksun-susfs-rc1.env" bash -c 'source "$0/scripts/common.sh"; printf "%s" "$SOURCE_DATE_EPOCH"' "$RELEASE_ROOT")"
 if [[ ! "$source_date_epoch" =~ ^[0-9]+$ ]] || (( source_date_epoch < 1700000000 )); then
   echo "SOURCE_DATE_EPOCH default is not a real timestamp: $source_date_epoch" >&2
   fail=1
